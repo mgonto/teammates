50e1df31011f4e9690a07237ecc976a187532b27
Guido Marucci Blas <guidomb@gmail.com>
2014-09-13 11:46:36 -0300

    Initial commit

---
+++ TeamMates/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TeamMates
+//
+//  Created by Guido Marucci Blas on 9/13/14.
+//
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
